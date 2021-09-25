48a2a487a04dce4dbcd2218b0c6fede68e733fea
Mr. T <43848500+teguhwin8@users.noreply.github.com>
2021-09-25 21:18:15 +0700

    make style for layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,24 @@
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core"
 import Head from 'next/head'
+import useStyles from "../utils/styles"
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+	const classes = useStyles()
+
 	return (
 		<div>
 			<Head>
 				<title>Next Commerce by Teguh Widodo</title>
 			</Head>
-			<AppBar position="static">
+			<AppBar position="static" className={classes.navbar}>
 				<Toolbar>
 					<Typography>NextCommerce</Typography>
 				</Toolbar>
 			</AppBar>
-			<Container>
+			<Container className={classes.main}>
 				{children}
 			</Container>
-			<footer>
+			<footer className="footer">
 				<Typography>All right reserved. Next Commerce.</Typography>
 			</footer>
 		</div>
