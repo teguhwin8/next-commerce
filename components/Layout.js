4668c4a863a1eb861963201e795bc7aceeeafb34
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-09-26 09:19:55 +0700

    add link to toolbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core"
-import Head from 'next/head'
+import { AppBar, Container, Link, Toolbar, Typography } from "@material-ui/core"
+import Head from "next/head"
 import useStyles from "../utils/styles"
+import NextLink from "next/link"
 
 export default function Layout({ children }) {
 	const classes = useStyles()
@@ -12,12 +13,27 @@ export default function Layout({ children }) {
 			</Head>
 			<AppBar position="static" className={classes.navbar}>
 				<Toolbar>
-					<Typography>NextCommerce</Typography>
+					<NextLink href="/" passHref>
+						<Link>
+							<Typography className={classes.brand}>NextCommerce</Typography>
+						</Link>
+					</NextLink>
+					<div className={classes.grow}></div>
+					<div>
+						<NextLink href="/cart" passHref>
+							<Link>
+								Cart
+							</Link>
+						</NextLink>
+						<NextLink href="/login" passHref>
+							<Link>
+								Login
+							</Link>
+						</NextLink>
+					</div>
 				</Toolbar>
 			</AppBar>
-			<Container className={classes.main}>
-				{children}
-			</Container>
+			<Container className={classes.main}>{children}</Container>
 			<footer className={classes.footer}>
 				<Typography>All right reserved. Next Commerce.</Typography>
 			</footer>
