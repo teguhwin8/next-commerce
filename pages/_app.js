3861d46732f89b3929e833bca6a0c3fc7a1da591
Mr. T <43848500+teguhwin8@users.noreply.github.com>
2021-09-26 21:09:31 +0700

    er

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
+<<<<<<< HEAD
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+=======
+import { useEffect } from "react"
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side")
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+>>>>>>> parent of aa0160e (I like tailwindcss)
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 
 export default function index() {
@@ -6,4 +7,54 @@ export default function index() {
       <h1>Hello World</h1>
     </div>
   )
+=======
+import {
+	Button,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Grid,
+	Typography,
+} from "@material-ui/core"
+import Layout from "../components/Layout"
+import data from "../utils/data"
+import NextLink from "next/link"
+
+export default function Home() {
+	return (
+		<Layout>
+			<div>
+				<h1>Products</h1>
+				<Grid container spacing={3}>
+					{data.products.map((product) => (
+						<Grid item md={4} key={product.name}>
+							<Card>
+								<NextLink href={`/product/${product.slug}`} passHref>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										image={product.image}
+										title={product.name}
+									></CardMedia>
+									<CardContent>
+										<Typography>{product.name}</Typography>
+									</CardContent>
+								</CardActionArea>
+								</NextLink>
+								<CardActions>
+									<Typography>{product.price}</Typography>
+									<Button size="small" color="primary">
+										Add To Cart
+									</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</div>
+		</Layout>
+	)
+>>>>>>> parent of aa0160e (I like tailwindcss)
 }
